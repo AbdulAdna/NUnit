fd74509f1a17a7217019ee3746d89c8bda50a18b
AbdulAdna <abdul.adna@gmail.com>
2023-05-08 10:45:57 +0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gradeCalculator = new GradeCalculator();
+
+            Console.WriteLine("Enter the percentage : ");
+            var percentage = Convert.ToInt32(Console.ReadLine());
+
+            var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+            Console.WriteLine($"Student grade : {grade}");
+        }
+    }
+}
